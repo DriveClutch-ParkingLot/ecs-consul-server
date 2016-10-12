e1fbc6ef589c875b7bf38f6ecee7007451aeeed0
David Hallum <david@kenzielane.com>
2016-10-12 07:04:19 -0400

    Adding -server switch [130376401]

--- ecs_entrypoint.sh
+++ ecs_entrypoint.sh
@@ -3,4 +3,4 @@
 IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | tr -d '\n')
 echo "IP=${IP}"
 
-docker-entrypoint.sh agent -client 0.0.0.0 -bootstrap-expect ${SERVER_COUNT} -atlas ${ENV_NAME} -atlas-join -atlas-token ${ATLAS_TOKEN}
+docker-entrypoint.sh agent -server -client 0.0.0.0 -bootstrap-expect ${SERVER_COUNT} -atlas ${ENV_NAME} -atlas-join -atlas-token ${ATLAS_TOKEN}
