fab9514517f9e724d3eae90793e982caa455dbd7
David Hallum <david@kenzielane.com>
2016-12-19 09:51:22 -0500

    Replacing atlas with ec2-tag, bumping to 0.7.1 version [#136354657]

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-FROM consul:v0.7.0
+FROM consul:v0.7.1
 MAINTAINER David Hallum <david@driveclutch.com>
 
 COPY ecs_entrypoint.sh /
 
 ENV SERVER_COUNT ""
-ENV DATACENTER ""
 ENV ENV_NAME ""
-ENV ATLAS_TOKEN ""
 ENV LOG_LEVEL "warn"
 
 CMD "/ecs_entrypoint.sh"
--- ecs_entrypoint.sh
+++ ecs_entrypoint.sh
@@ -3,14 +3,12 @@
 IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | tr -d '\n')
 echo "IP=${IP}"
 
-docker-entrypoint.sh agent          \
-  -server                           \
-  -ui                               \
-  -advertise=$IP                    \
-  -client=0.0.0.0                   \
-  -bootstrap-expect=${SERVER_COUNT} \
-  -datacenter=${DATACENTER}         \
-  -atlas=${ENV_NAME}                \
-  -atlas-join                       \
-  -atlas-token=${ATLAS_TOKEN}       \
+docker-entrypoint.sh agent               \
+  -server                                \
+  -ui                                    \
+  -advertise=$IP                         \
+  -client=0.0.0.0                        \
+  -bootstrap-expect=${SERVER_COUNT}      \
+  -retry-join-ec2-tag-key="consulenv"    \
+  -retry-join-ec2-tag-value==${ENV_NAME} \
   -log-level=${LOG_LEVEL}
